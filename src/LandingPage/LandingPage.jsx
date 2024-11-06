a9a76c4d2e859b63f25ddd5112d09b2e53f9d505
Rony7736 <ronychakrabarty99@gmail.com>
2024-11-06 23:44:27 +0600

    added helmet

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Gadget Heaven Project</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+            </Helmet>
+            <h1>Gadget Heaven Project</h1>
+        </div>
+    );
+};
+
+export default LandingPage;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Toaster } from 'react-hot-toast';
 import { createContext, useState } from "react";
+import { HelmetProvider } from "react-helmet-async";
 
 export const MyContext = createContext(null)
 export default function MainLayout() {
@@ -13,28 +14,36 @@ export default function MainLayout() {
         setPriceContext,
     }
 
-    // const priceList = (p) => {
-    //     setPrice(price + p)
-    // }
     return (
-        <div>
-            <Toaster></Toaster>
-            <div className="container mx-auto px-6 bg-[#F7F7F7] mt-6 h-16">
-                {/* Navbar */}
-                <Navbar></Navbar>
+
+        <HelmetProvider>
+
+            <div>
+                <Toaster></Toaster>
+
+                <MyContext.Provider value={priceLoad}>
+
+                    <div className="container mx-auto px-6 bg-[#F7F7F7] mt-6 h-16">
+                        {/* Navbar */}
+                        <Navbar></Navbar>
+                    </div>
+
+                    <div className="container mx-auto min-h-[calc(100vh-538px)] bg-[#F7F7F7] px-6 pb-12">
+                        {/* Dunamic section */}
+                        <Outlet></Outlet>
+                    </div>
+
+                    <div className="container mx-auto">
+                        {/* Footer */}
+                        <Footer></Footer>
+                    </div>
+
+                </MyContext.Provider>
+
+
             </div>
 
-            <MyContext.Provider value={priceLoad}>
-                <div className="container mx-auto min-h-[calc(100vh-538px)] bg-[#F7F7F7] px-6 pb-12">
-                    {/* Dunamic section */}
-                    <Outlet></Outlet>
-                </div>
-            </MyContext.Provider>
+        </HelmetProvider>
 
-            <div className="container mx-auto">
-                {/* Footer */}
-                <Footer></Footer>
-            </div>
-        </div>
     );
 };
